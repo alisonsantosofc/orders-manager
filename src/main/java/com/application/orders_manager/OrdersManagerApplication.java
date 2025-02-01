b0f4a83ff235b63b2642efec045e40a38111d8b4
alisonsantosofc <alisonsantoso22@gmail.com>
2025-02-01 10:15:04 -0300

    build: init project

---
+++ src/main/java/com/application/orders_manager/OrdersManagerApplication.java
@@ -0,0 +1,11 @@
+package com.application.orders_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/application/orders_manager/OrdersManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.orders_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
